8dfb3fa6e9f5173b094ccebe00d4844162661859
Jorgpro <115546930+Jorgpro@users.noreply.github.com>
2022-12-01 20:49:40 -0300

    Add files via upload

---
+++ curso3.js
@@ -0,0 +1,19 @@
+
+let nombre= "Jorge";
+let apellido="Zapata";
+let estudiante="Jorge"+" "+"Zapata";
+let estudianteMayus= "ESTUDIANTE";
+let estudianteMinus="estudiante";
+let numEst= 12;
+let name="J";
+let aPP= "a";
+let cadenaDeTexto= "JorgeZapata";
+let Nombre= true;
+
+console.log(estudiante.length)
+console.log(nombre.concat(" ",apellido));
+console.log(estudiante.includes("Jorge"));
+console.log(nombre.startsWith("J"));
+console.log(apellido.endsWith("a"));
+console.log(estudiante.trimStart().length);
+
